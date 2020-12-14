7c3cd344e1d72a6519c357678153a5d5574cd872
Youssouf Donzo <donzoyoussouf@gmail.com>
2020-12-14 17:33:16 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use App\Models\{Methode, Categorie};
-use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        View::share('methodes', Methode::get());
-        View::share('top_level_categories', Categorie::whereNull('id_parent')->inRandomOrder()->get());
-        View::share('all_categories', Categorie::inRandomOrder()->get());
-
     }
 }
